501ccc46f921311922692dab67ac43b0c0f07003
unizinha <unizinha.minhapica@gmail.com>
2026-06-30 17:37:45 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -362,7 +362,7 @@ export const botConfig = {
     // Welcome template posted when a user joins.
     // Placeholders: {user}, {server}, {memberCount}
     defaultWelcomeMessage:
-      "Welcome {user} to {server}! We now have {memberCount} members!",
+      "Bem-Vindo(a)! {user} ao {server}!",
     // Goodbye template posted when a user leaves.
     // Placeholders: {user}, {memberCount}
     defaultGoodbyeMessage:
